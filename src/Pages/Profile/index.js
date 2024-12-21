0aff675ee58ddf5a8772751a226db81e5598c6de
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2024-12-21 11:46:03 +0530

    added change profile pic

--- src/Pages/Profile/index.js
+++ src/Pages/Profile/index.js
@@ -18,7 +18,9 @@ import { Helmet } from 'react-helmet';
 import LockIcon from '@mui/icons-material/Lock';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import useFetchDBData from '../../hooks/useFetchDBData';
-
+import { Modal } from 'react-bootstrap';
+import CloseIcon from '@mui/icons-material/Close';
+import ModeIcon from '@mui/icons-material/Mode';
 export default function Profile({ setBackdrop, scrollTop }) {
 
   const [watchlist, setWatchlist] = useState([])
@@ -34,6 +36,40 @@ export default function Profile({ setBackdrop, scrollTop }) {
   const [publicAcc, setPublicAcc] = useState(true)
   const [watchedCount, setWatchedCount] = useState(0)
   const [favouriteCount, setFavouriteCount] = useState(0)
+
+  const currentPhoto = useFetchUserDetails(auth?.currentUser?.uid, 'photo')
+  const currentUsername = useFetchUserDetails(auth?.currentUser?.uid, 'username')
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const images = [
+    `${process.env.PUBLIC_URL}/1.jpeg`,
+    `${process.env.PUBLIC_URL}/2.jpeg`,
+    `${process.env.PUBLIC_URL}/3.jpeg`,
+    `${process.env.PUBLIC_URL}/4.jpeg`,
+    `${process.env.PUBLIC_URL}/5.jpeg`,
+    `${process.env.PUBLIC_URL}/6.jpeg`,
+    `${process.env.PUBLIC_URL}/7.jpeg`,
+    `${process.env.PUBLIC_URL}/8.jpeg`,
+    `${process.env.PUBLIC_URL}/9.jpeg`,
+    `${process.env.PUBLIC_URL}/10.jpeg`,
+    `${process.env.PUBLIC_URL}/11.jpeg`,
+    `${process.env.PUBLIC_URL}/12.jpeg`,
+    `${process.env.PUBLIC_URL}/13.jpeg`,
+    `${process.env.PUBLIC_URL}/14.jpeg`,
+    `${process.env.PUBLIC_URL}/15.jpeg`,
+    `https://api.dicebear.com/9.x/dylan/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/adventurer/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/adventurer-neutral/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/fun-emoji/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/micah/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/open-peeps/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/thumbs/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/notionists/svg?seed=${currentUsername}?size=96`,
+    `https://api.dicebear.com/9.x/initials/svg?seed=${currentUsername}?size=96`,
+  ]
 
   const watching = useFetchDBData(auth?.currentUser?.uid, 'watching')
 
@@ -75,9 +111,6 @@ export default function Profile({ setBackdrop, scrollTop }) {
       })
     }
   }
-
-  const currentPhoto = useFetchUserDetails(auth?.currentUser?.uid, 'photo')
-  const currentUsername = useFetchUserDetails(auth?.currentUser?.uid, 'username')
 
   useEffect(() => {
     database.ref(`/Users/${auth?.currentUser?.uid}/premium`).on('value', snapshot => {
@@ -130,15 +163,49 @@ export default function Profile({ setBackdrop, scrollTop }) {
     setLoading(false)
   }, [auth?.currentUser?.uid])
 
+  const handleChangePicture = (image) => {
+    database.ref(`/Users/${auth?.currentUser?.uid}`).update({
+      photo: image
+    }).then(() => {
+      console.log('Picture updated')
+      handleClose()
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
+
   return (
     <>
       <Helmet>
         <title>SiyaCine{currentUsername ? ` - ${currentUsername}` : ''}</title>
       </Helmet>
+
+      <Modal size='md' show={show} onHide={handleClose} centered>
+        <Modal.Body style={{ backgroundColor: theme.palette.background.default }}>
+          <div className='modal_header'>
+            <h2>Choose a different avatar</h2>
+            <IconButton onClick={() => handleClose()}><CloseIcon style={{ color: 'red' }} /></IconButton>
+          </div>
+          <div>
+            <div className='picture_container'>
+              {images?.map((img) => {
+                return (
+                  <div>
+                    <img onClick={() => handleChangePicture(img)} className='picture_single' src={img} />
+                    {img === currentPhoto && <div className='current'>Current</div>}
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
       {!loading ? <div className='profile'>
         <div className='profile_header'>
           <div className='pic_container'>
-            <img src={currentPhoto ? currentPhoto : 'https://api.dicebear.com/8.x/fun-emoji/svg?seed=fun?size=96'} className='profile_image' />
+            <img src={currentPhoto ? currentPhoto : 'https://api.dicebear.com/8.x/fun-emoji/svg?seed=fun?size=96'} className='profile_image hovereffect' />
+            <IconButton onClick={handleShow} className='edit_icon'><ModeIcon /></IconButton>
           </div>
           <div className='profile_right'>
             <h1>{currentUsername ? currentUsername : 'Loading...'}</h1>
