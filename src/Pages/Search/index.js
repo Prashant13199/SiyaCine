09e859fe7c35affc782505cc4f8eb131f2515e3e
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2025-04-05 19:13:07 +0530

    search title and length

--- src/Pages/Search/index.js
+++ src/Pages/Search/index.js
@@ -54,7 +54,7 @@ export default function Search({ scrollTop, setBackdrop }) {
             snapshot.forEach((snap) => {
                 arr.push(snap.val())
             })
-            setRecentlySearched(arr.reverse().splice(0, 12))
+            setRecentlySearched(arr.reverse().splice(0, 18))
         })
     }
 
@@ -121,7 +121,7 @@ export default function Search({ scrollTop, setBackdrop }) {
                         </IconButton>}
                 </Paper>
                 <br />
-                {!query && recentlySearched?.length > 0 && <div className='search_title'>Recently searched</div>}
+                {!query && recentlySearched?.length > 0 && <div className='discover_movies_title'>Recently searched</div>}
                 <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 6, sm: 12, md: 24 }}>
                     {page == 1 && searchedUsers?.length > 0 &&
                         <>
