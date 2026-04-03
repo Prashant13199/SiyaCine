655d9a2c4ac74036293703504615a892e2821971
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2026-04-03 17:57:48 +0530

    added careusol

---
+++ src/Components/SingleSlide/SingleSlide.js
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import useFetchContent from '../../hooks/useFetchContent';
+import './style.css';
+
+export default function SingleSlide({ data, index }) {
+
+    const nowPlaying = useFetchContent('now_playing', 'movie')
+
+    const [title, setTitle] = useState('')
+
+    useEffect(() => {
+        axios.get(`https://api.themoviedb.org/3/movie/${nowPlaying?.[index]?.id}/images?api_key=${process.env.REACT_APP_API_KEY}&include_image_language=en`)
+            .then(response => {
+                setTitle(response.data.logos[0].file_path);
+            })
+            .catch(error => {
+                console.error('Error fetching now playing movies:', error);
+            });
+    }, [index, nowPlaying])
+
+    return (
+        <div className="embla__slide" key={index}>
+            <Link to={`/singlecontent/${data?.id}/movie`} style={{ textDecoration: 'none' }}>
+                <img
+                    className="embla__slide__img"
+                    src={data?.length !== 0 ? `https://image.tmdb.org/t/p/original/${window.innerWidth > 900 ? data?.backdrop_path : data?.poster_path}` : 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(231,145,10,1) 0%, rgba(255,0,187,1) 100%)'}
+                    alt="Your alt text"
+                />
+                {title && <div className='welcome_text'>
+                    <img src={`https://image.tmdb.org/t/p/original/${title}`} className='title_image' />
+                </div>}
+            </Link>
+        </div>
+    )
+}
--- src/Containers/Header/index.js
+++ src/Containers/Header/index.js
@@ -1,35 +1,25 @@
-import React, { useState } from 'react';
 import './style.css';
-import { Link } from 'react-router-dom';
-import Carousel from 'react-bootstrap/Carousel';
+
 import useFetchContent from '../../hooks/useFetchContent';
+import useEmblaCarousel from 'embla-carousel-react'
+import ClassNames from 'embla-carousel-class-names'
+import SingleSlide from '../../Components/SingleSlide/SingleSlide';
 
 export default function Header() {
 
-  const [index, setIndex] = useState(0);
+  const [emblaRef] = useEmblaCarousel({}, [ClassNames()])
 
   const nowPlaying = useFetchContent('now_playing', 'movie')
 
-  const handleSelect = (selectedIndex) => {
-    setIndex(selectedIndex);
-  };
-
   return (
-    <Carousel activeIndex={index} touch={true} onSelect={handleSelect} controls={false} indicators={false}>
-      {nowPlaying?.map((data) => {
-        return <Carousel.Item key={data?.id}>
-          <Link to={`/singlecontent/${data?.id}/movie`} style={{ textDecoration: 'none' }}>
-            <div className='welcome' style={{ backgroundImage: data?.length !== 0 ? `url(https://image.tmdb.org/t/p/original/${window.innerWidth > 900 ? data?.backdrop_path : data?.poster_path})` : 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(231,145,10,1) 0%, rgba(255,0,187,1) 100%)' }}>
-              <div className='welcome_backdrop'>
-                <Carousel.Caption className='welcome_text'>
-                  <h3>{data?.title}</h3>
-                  <p>{data?.overview.substring(0, 100).concat('...')}</p>
-                </Carousel.Caption>
-              </div>
-            </div>
-          </Link>
-        </Carousel.Item>
-      })}
-    </Carousel>
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {nowPlaying?.map((data, index) => (
+            <SingleSlide data={data} index={index} />
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
