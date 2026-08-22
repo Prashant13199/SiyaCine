e8e24cd70564dc6a71044261410e69103221136a
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2026-08-22 22:16:36 +0530

    live update c

--- src/Containers/Live/Live.js
+++ src/Containers/Live/Live.js
@@ -28,7 +28,11 @@ export default function Live() {
         { id: 3, name: "NDTV", url: "https://www.youtube.com/watch?v=8ZqHaMASZd8", photo: "https://i3.ytimg.com/vi/8ZqHaMASZd8/maxresdefault.jpg" },
         { id: 4, name: "Pop Music", url: "https://www.youtube.com/watch?v=3MOrgUjiigE", photo: "https://i3.ytimg.com/vi/3MOrgUjiigE/maxresdefault.jpg" },
         { id: 5, name: "Lofi Mashup", url: "https://www.youtube.com/watch?v=2RGXX8QQk8g", photo: "https://i3.ytimg.com/vi/2RGXX8QQk8g/maxresdefault.jpg" },
-        { id: 6, name: "Summer Mix", url: "https://www.youtube.com/watch?v=dnpRUk2be84", photo: "https://i3.ytimg.com/vi/dnpRUk2be84/maxresdefault.jpg" }
+        { id: 6, name: "Summer Mix", url: "https://www.youtube.com/watch?v=dnpRUk2be84", photo: "https://i3.ytimg.com/vi/dnpRUk2be84/maxresdefault.jpg" },
+        { id: 7, name: "Tom & Jerry", url: "https://www.youtube.com/watch?v=rEKifG2XUZg", photo: "https://i3.ytimg.com/vi/rEKifG2XUZg/maxresdefault.jpg" },
+        { id: 8, name: "Oggy and the Cockroaches", url: "https://www.youtube.com/watch?v=1R8767hzeLk", photo: "https://i3.ytimg.com/vi/1R8767hzeLk/maxresdefault.jpg" },
+        { id: 9, name: "Harry Potter", url: "https://www.youtube.com/watch?v=JSVny7yLc3s", photo: "https://i3.ytimg.com/vi/JSVny7yLc3s/maxresdefault.jpg" },
+        { id: 10, name: "The Office", url: "https://www.youtube.com/watch?v=AJmaVPfyudQ", photo: "https://i3.ytimg.com/vi/AJmaVPfyudQ/maxresdefault.jpg" }
     ]
 
     return (
