ea866ebe61c300b7118e919da73d2840549d7c86
Prashant Kumar <prashantkumar@Prashants-MacBook-Pro.local>
2026-08-22 22:04:22 +0530

    live update n

--- src/Containers/Live/Live.js
+++ src/Containers/Live/Live.js
@@ -25,7 +25,7 @@ export default function Live() {
     const data = [
         { id: 1, name: "Aaj Tak", url: "https://www.youtube.com/watch?v=Nq2wYlWFucg", photo: "https://i3.ytimg.com/vi/Nq2wYlWFucg/maxresdefault.jpg" },
         { id: 2, name: "India TV", url: "https://www.youtube.com/watch?v=YIkoXVyVOSI", photo: "https://i3.ytimg.com/vi/YIkoXVyVOSI/maxresdefault.jpg" },
-        { id: 3, name: "India TV", url: "https://www.youtube.com/watch?v=8ZqHaMASZd8", photo: "https://i3.ytimg.com/vi/8ZqHaMASZd8/maxresdefault.jpg" },
+        { id: 3, name: "NDTV", url: "https://www.youtube.com/watch?v=8ZqHaMASZd8", photo: "https://i3.ytimg.com/vi/8ZqHaMASZd8/maxresdefault.jpg" },
         { id: 4, name: "Pop Music", url: "https://www.youtube.com/watch?v=3MOrgUjiigE", photo: "https://i3.ytimg.com/vi/3MOrgUjiigE/maxresdefault.jpg" },
         { id: 5, name: "Lofi Mashup", url: "https://www.youtube.com/watch?v=2RGXX8QQk8g", photo: "https://i3.ytimg.com/vi/2RGXX8QQk8g/maxresdefault.jpg" },
         { id: 6, name: "Summer Mix", url: "https://www.youtube.com/watch?v=dnpRUk2be84", photo: "https://i3.ytimg.com/vi/dnpRUk2be84/maxresdefault.jpg" }
